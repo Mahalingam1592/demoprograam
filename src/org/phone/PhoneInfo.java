ac2682d4f106aa9a7849cb729914ca7da4ec5277
mahalingam <mahalingamsubramniyan@gmail.com>
2021-08-09 21:35:36 +0530

    hi sri

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,38 @@
+package org.phone;
+
+public class PhoneInfo {
+	private void phoneName() {
+	System.out.println("POCO");
+
+	}
+	
+	private void phoneImei() {
+		System.out.println("12345677890");
+
+	}
+	
+	private void Storage() {
+		System.out.println("16gb");
+
+	}
+	
+	private void camera() {
+		System.out.println("64mp");
+
+	}
+	
+	private void osName() {
+		System.out.println("android");
+
+	}
+public static void main(String[] args) {
+	PhoneInfo c=new PhoneInfo();
+	c.camera();
+	c.osName();c.phoneImei();
+	c.phoneName();
+	c.Storage();
+	
+	
+}	
+
+}
